b1a3af95295c78e6d6c1b2af0699d49b66883c9c
ragasIntercom <wd2.intercom@gmail.com>
2026-05-15 20:42:53 +0700

    refactor program cs and renew docs.md

--- Program.cs
+++ Program.cs
@@ -1,19 +1,86 @@
-using Microsoft.EntityFrameworkCore;
 using DotnetCRUD.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-
-builder.Services.AddEndPointsApiExplorer();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSwaggerGen();
 
 // Database setup
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+
+// Controllers
+builder.Services.AddControllers();
+
+//CORS
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+//JWT Authentication
+var jwtKey = builder.Configuration["Jwt:Key"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+
+            IssuerSigningKey = 
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtKey!)
+                    ),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+// Swagger + JWT
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Schema = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Input JWT Token"
+    });
+
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 // Dependency Injection
 
@@ -29,7 +96,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors("AllowFrontEnd");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
--- docs.md
+++ docs.md
@@ -1,6 +1,6 @@
 step belaja DOTNEt:
-1. setup program.cs
-2. atur folder :
+
+1. atur folder :
 	`-bin,
 	`-Controllers,
 	`-Data,
@@ -10,13 +10,14 @@ step belaja DOTNEt:
 	`-Properties,
 	`-Repositories,
 	`-Services,
-3. install pack:
+2. install pack:
 	`-dotnet add package Microsoft.EntityFrameworkCore
 	`-dotnet add package Microsoft.EntityFrameworkCore.Design
 	`-dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL
 	`-dotnet add package Swashbuckle.AspNetCore
 	`-dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer
 	`-dotnet add package BCrypt.Net-Next
-4. setup appsettings.json
-5. buat model, DTO, repository, service, controller
+3. setup appsettings.json
+4. buat model, DTO, repository, service, controller
+5. setup program.cs
 6. buat migration dan update database
